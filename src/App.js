214e6976d6710d4b0586d089d3be0b3a60110915
OpondoG <okonjogilbert2@gmail.com>
2022-11-04 15:08:02 +0300

    Initialize app with components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/NavBar';
+import Books from './Pages/books';
+import Categories from './Pages/category';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </BrowserRouter>
+  </>
   );
 }
 
